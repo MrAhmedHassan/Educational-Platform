fb8075ddf7d2bb36b60364215aff7ee44b54b115
MahmoudRagab <MahmoudRagab0055@gmail.com>
2020-01-10 21:49:59 +0200

    authorization added

---
+++ master/app/Http/Controllers/HomeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+    //    for create role
+    //    Role::create(['name'=>'Admin']);
+    //    Role::create(['name'=>'Teacher']);
+
+    //    for create permission
+    //    Permission::create(['name'=>'create teacher']);
+    //    Permission::create(['name'=>'create supporter']);
+    //    Permission::create(['name'=>'remove teacher']);
+    //     Permission::create(['name' => 'remove supporter']);
+
+    //      Role::create(['name'=> 'supporter']);
+        //   $supporter = Role::findById(3);
+        //   $supporter->givePermissionTo(Permission::findById(4));
+//        for done the relation and connect the role and permission
+        // $admin = Role::findById(1);
+    //    $admin->givePermissionTo('create teacher');
+    //    $teacher = Role::findById(2);
+    //    $teacher->givePermissionTo('create supporter');
+    //    $admin->givePermissionTo(Permission::findById(1));
+    //    or
+    //    $removeTeacher = Permission::findById(3);
+    //    $removeTeacher->assignRole('Admin');
+
+    //    To Remove Permission
+    //    $admin->revokePermissionTo('create supporter');
+    //  $removeTeacher->removeRole('Admin');
+        return view('home');
+    }
+}
---
+++ master/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="all_content" style="margin-top: 65px;margin-left: 270px">
+
+<div class="bg-dark w-50 h-50">CONTENT HERE</div>
+<br>
+<h1>YEHIA STARTS HERE</h1>
+
+
+</div>
+
+
+
+@endsection
